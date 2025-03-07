fba1e8c311f07c6dcd4ed3e29803d6ca60365230
Mujtaba Qureshi <mujtabaqureshi009@gmail.com>
2025-03-07 19:40:30 +0500

    Create Task 1.cpp

---
+++ Lab 06/Task 1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class Employee {
+protected:
+    string name;
+    float salary;
+
+public:
+    Employee(string n, float s) : name(n) , salary(s) {}
+
+    void displayDetails() {
+        cout << "Name: " << name << endl;
+        cout << "Salary: $" << salary << endl;
+    }
+};
+
+class Manager : public Employee {
+private:
+    float bonus;
+
+public:
+    Manager(string n, float s, float b) : Employee(n, s) , bonus(b) {}
+
+    void displayManagerDetails() {
+        displayDetails();
+        cout << "Bonus: $" << bonus << endl;
+    }
+};
+
+int main() {
+    Manager mgr("Mujtaba",50000.0,5000.0);
+    cout << "\nManager Details:\n";
+    mgr.displayManagerDetails();
+
+    return 0;
+}
